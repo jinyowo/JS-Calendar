a041b50eeeb37bf75374c62f3d1ce85c339dff07
jiny <hjin421@gmail.com>
2017-01-19 16:13:53 +0900

    feat(moveMonthEvent): 화살표 버튼으로 달 이동하는 기능 구현
    화살표 버튼을 눌러서 달력을 바꾸는 기능을 구현했습니다.
    월의 범위를 넘어가는 부분 예외처리를 추가했습니다.
    
    issue #6

--- template/lib/month-calendar.js
+++ template/lib/month-calendar.js
@@ -2,41 +2,32 @@
 var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 var weekdayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var weekdayClassArray = ["fc-sun", "fc-mon", "fc-tue", "fc-wed", "fc-thu", "fc-fri", "fc-sat"];
-// 1. 처음 캘린더를 켰을 때 : 오늘 날짜가 속해 있는 달의 날짜를 채움
-// 2. 화살표 : 오늘을 기준으로 달을 +, - 해서 그 달의 날짜를 채움
-// 3. 각각의 날짜는 달력의 칸마다 오브젝트 형태로 저장해서 클릭이벤트시 주고받을 수 있도록함.(fc-day의 data-date속성 이용)
 
-// 오늘의 날짜를 받아옵니다.
-var today = new Date();
-var thisYear = today.getFullYear();
-var thisMonth = today.getMonth(); // 0 ~ 11
-var thisDate = today.getDate();
-
-
+var myMonth = -1;
+var myYear = -1;
 function getLastDate(month) {
     if(month < 0){ month = 11;}
 
-    var lastDate = new Date(thisYear, month+1, 0).getDate();
+    var lastDate = new Date(myYear, month+1, 0).getDate();
     return lastDate;
 }
 
-setCalendar(thisYear, thisMonth);
-
 // 월에 맞도록 달력에 숫자를 뿌리는 함수
-function setCalendar(targetYear, targetMonth) {
+function setCalendar() {
     // 해당 년, 월의 1일 구하기
-    var firstDate = new Date(targetYear, targetMonth, 1); // 이번달 1일
-    var lastDate = getLastDate(targetMonth);
+    var firstDate = new Date(myYear, myMonth, 1); // 이번달 1일
+
+    var lastDate = getLastDate(myMonth);
     var firstWeekday = firstDate.getDay();
 
     // 상단 가운데 월이름, 년도 -TODO: 나중에 함수로 뺄 예정
     var monthTitle = document.querySelector(".fc-center");
-    var thisMonthFullname = monthArray[targetMonth];
-    monthTitle.innerHTML = "<h2>"+thisMonthFullname+" "+targetYear+"</h2>";
+    var thisMonthFullname = monthArray[myMonth];
+    monthTitle.innerHTML = "<h2>"+thisMonthFullname+" "+myYear+"</h2>";
 
     // 달력에 숫자 뿌리기 - TODO: 함수로 빼기
     var calendar = [];
-    var prevMonthLastDate = getLastDate(targetMonth-1);
+    var prevMonthLastDate = getLastDate(myMonth-1);
     var prevMonthfirstDate = prevMonthLastDate - firstWeekday + 1;
 
     // 지난달에 해당하는 날짜를 먼저 배열에 넣어준다.
@@ -64,7 +55,42 @@ function saveDate(date) {
 
 }
 
+function moveMonth(evt) {
+    var prevArrowClass = "fc-prev-button";
+    var nextArrowClass = "fc-next-button";
 
+    var button = evt.target.closest("button");
+
+    if(button.classList.contains(prevArrowClass)) {
+        myMonth--;
+    } else if(button.classList.contains(nextArrowClass)) {
+        myMonth++;
+    }
+
+    if(myMonth < 0) { myMonth = 11; myYear--;}
+    else if(myMonth > 11) { myMonth = 0; myYear++;}
+    
+    setCalendar();
+}
+function setMyDate(year, month) {
+    myMonth = month;
+    myYear = year;
+}
+document.addEventListener("DOMContentLoaded", init);
+function init() {
+    // 오늘의 날짜를 받아옵니다.
+    var today = new Date();
+    var thisYear = today.getFullYear();
+    var thisMonth = today.getMonth(); // 0 ~ 11
+    var thisDate = today.getDate();
+
+    setMyDate(thisYear, thisMonth);
+    setCalendar();
+
+    // 화살표 달력 이동
+    var arrowButtons = document.querySelector(".fc-left .fc-button-group");
+    arrowButtons.addEventListener("click", moveMonth);
+}
 
 // 임시
 document.querySelector(".fc-basicWeek-view").style.display = "none";
