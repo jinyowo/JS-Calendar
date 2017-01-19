9d7cc198c3ec244ca5c1e6e9b22c37b4e67934c8
jiny <hjin421@gmail.com>
2017-01-19 11:56:26 +0900

    feat(month-calendar): 날짜를 계산하여 달력에 뿌리는 기능 구현
    - 년과 월에 따라 해당 월의 달력을 출력하는 함수를 구현.
    - 오늘의 날짜에 따라 오늘에 해당하는 달력을 출력하게 되어있음(현재).
    - 아직 윤년에 대한부분은 구현x
    
    issue#2, #3

--- template/lib/month-calendar.js
+++ template/lib/month-calendar.js
@@ -1,33 +1,70 @@
+// month이름, weekday이름, 각 달의 마지막 날짜를 저장한 배열
 var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 var weekdayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
+var lastDateArray = [31,28,31,30,31,30,31,31,30,31,30,31];
+var weekdayClassArray = ["fc-sun", "fc-mon", "fc-tue", "fc-wed", "fc-thu", "fc-fri", "fc-sat"];
+// 1. 처음 캘린더를 켰을 때 : 오늘 날짜가 속해 있는 달의 날짜를 채움
+// 2. 화살표 : 오늘을 기준으로 달을 +, - 해서 그 달의 날짜를 채움
+// 3. 각각의 날짜는 달력의 칸마다 오브젝트 형태로 저장해서 클릭이벤트시 주고받을 수 있도록함.(fc-day의 data-date속성 이용)
 
 // 오늘의 날짜를 받아옵니다.
-var date = new Date();
-var y = date.getFullYear();
-var m = date.getMonth();
-var d = date.getDate();
+var today = new Date();
+var thisYear = today.getFullYear();
+var thisMonth = today.getMonth();
+var thisDate = today.getDate();
 
-var fullMonthName = monthArray[m];
+function getLastDate(month) {
+    if(month < 0){ month = 11;}
 
-// 이번달 1일
-var firstDate = new Date(y, m, 1);
-var firstDay = weekdayArray[firstDate.getDay()];
+    var lastDate = lastDateArray[month];
+    // TODO 윤년계산
 
-var last = [31,28,31,30,31,30,31,31,30,31,30,31];
-var lastDate = last[m];
+    return lastDate;
+}
 
-// 4년마다 있는 윤년을 계산합니다.(100년||400년 주기는 제외)
+// setCalendar(thisYear, thisMonth);
 
-// .fc-center : 상단 가운데 월이름, 년도
-var monthTitle = document.querySelector(".fc-center");
-monthTitle.innerHTML = "<h2>"+fullMonthName+" "+y+"</h2>";
+setCalendar(2017, 4);
 
-//달력 숫자 표시 .fc-day-grid > .fc-content-skeleton > thead > tr > td.fc-day-top > a
-var rows = document.querySelectorAll(".fc-month-view .fc-day-grid .fc-row");
-var firstRow = rows.firstElementNodes;
-console.log(firstRow.childNodes);
+// 월에 맞도록 달력에 숫자를 뿌리는 함수
+function setCalendar(targetYear, targetMonth) {
+    // 해당 년, 월의 1일 구하기
+    var firstDate = new Date(targetYear, targetMonth, 1); // 이번달 1일
+    var lastDate = getLastDate(targetMonth);
+    var firstWeekday = firstDate.getDay();
 
+    // 상단 가운데 월이름, 년도 -TODO: 나중에 함수로 뺄 예정
+    var monthTitle = document.querySelector(".fc-center");
+    var thisMonthFullname = monthArray[targetMonth];
+    monthTitle.innerHTML = "<h2>"+thisMonthFullname+" "+targetYear+"</h2>";
+
+    // 달력에 숫자 뿌리기 - TODO: 함수로 빼기
+    var calendar = [];
+    var prevMonthLastDate = getLastDate(targetMonth-1);
+    var prevMonthfirstDate = prevMonthLastDate - firstWeekday + 1;
+    // 지난달에 해당하는 날짜를 먼저 배열에 넣어준다.
+    for(var i = prevMonthfirstDate; i<=prevMonthLastDate; i++) {
+        calendar.push(i);
+    }
+    // 이번달에 해당하는 날짜를 추가로 배열에 넣어준다.
+    for(var i = 1; i<=lastDate; i++) {
+        calendar.push(i);
+    }
+    // 숫자를 띄울 a태그 전체
+    var nums = document.querySelectorAll(".fc-content-skeleton a.fc-day-number");
+    var nextMonthDate = 1;
+    for(var i=0; i<nums.length; i++) {
+        // 다음달 날짜 채우기
+        if(calendar[i] === undefined) {
+            calendar[i] = nextMonthDate++;
+        }
+        nums[i].innerText = calendar[i];
+    }
+}
+// 해당 날짜에 대한 정보를 ISO형식으로 div의 data-date속성으로 저장
+function saveDate(date) {
+
+}
 
 
 
