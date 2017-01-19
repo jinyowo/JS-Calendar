a2f40beaacce472806f70eb980900d56f4778ede
jiny <hjin421@gmail.com>
2017-01-19 14:34:48 +0900

    refactor(month-calendar): 해당 달의 마지막 날짜 getDate함수로 바꾸어 구현

--- template/lib/month-calendar.js
+++ template/lib/month-calendar.js
@@ -1,7 +1,6 @@
 // month이름, weekday이름, 각 달의 마지막 날짜를 저장한 배열
-var monthArray = ['','January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 var weekdayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var lastDateArray = [0,31,28,31,30,31,30,31,31,30,31,30,31];
 var weekdayClassArray = ["fc-sun", "fc-mon", "fc-tue", "fc-wed", "fc-thu", "fc-fri", "fc-sat"];
 // 1. 처음 캘린더를 켰을 때 : 오늘 날짜가 속해 있는 달의 날짜를 채움
 // 2. 화살표 : 오늘을 기준으로 달을 +, - 해서 그 달의 날짜를 채움
@@ -10,27 +9,23 @@ var weekdayClassArray = ["fc-sun", "fc-mon", "fc-tue", "fc-wed", "fc-thu", "fc-f
 // 오늘의 날짜를 받아옵니다.
 var today = new Date();
 var thisYear = today.getFullYear();
-var thisMonth = today.getMonth() + 1; // 0 ~ 11
+var thisMonth = today.getMonth(); // 0 ~ 11
 var thisDate = today.getDate();
 
-function getLastDate(month) {
-    if(month < 1){ month = 12;}
 
-    var lastDate = lastDateArray[month];
-    if(month===2 && ((thisYear%4===0 && thisYear%100 !== 0) || thisYear%400===0)) {
-        lastDate = 29;
-    }
+function getLastDate(month) {
+    if(month < 0){ month = 11;}
+
+    var lastDate = new Date(thisYear, month+1, 0).getDate();
     return lastDate;
 }
 
-//setCalendar(thisYear, thisMonth);
-
-setCalendar(2017, 4);
+setCalendar(thisYear, thisMonth);
 
 // 월에 맞도록 달력에 숫자를 뿌리는 함수
 function setCalendar(targetYear, targetMonth) {
     // 해당 년, 월의 1일 구하기
-    var firstDate = new Date(targetYear, targetMonth-1, 1); // 이번달 1일
+    var firstDate = new Date(targetYear, targetMonth, 1); // 이번달 1일
     var lastDate = getLastDate(targetMonth);
     var firstWeekday = firstDate.getDay();
 
@@ -43,6 +38,7 @@ function setCalendar(targetYear, targetMonth) {
     var calendar = [];
     var prevMonthLastDate = getLastDate(targetMonth-1);
     var prevMonthfirstDate = prevMonthLastDate - firstWeekday + 1;
+
     // 지난달에 해당하는 날짜를 먼저 배열에 넣어준다.
     for(var i = prevMonthfirstDate; i<=prevMonthLastDate; i++) {
         calendar.push(i);
