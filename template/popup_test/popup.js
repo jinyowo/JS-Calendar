c267c99b18826c992a625b775a863cb6c2284fc9
likelion-lulushu <iampoem315@gmail.com>
2017-01-21 16:13:38 +0900

    feat(popup_test):fc-content 클릭시 좌표와 일정object 반영

--- template/popup_test/popup.js
+++ template/popup_test/popup.js
@@ -1,26 +1,60 @@
-var schedule={ };
-var popup = document.getElementById('popup');
+//테스트용 object
+var schedule = {
+    title: "NTS intern",
+    start: "2017-01-02T08:00",
+    end: "2017-02-10T19:00",
+    repeat: "W",
+    place: "startup campus",
+    desc: "FE 인턴 education"
+};
+//반복 주기 object
+var repeat = {
+    Y: "매년",
+    M: "매월",
+    W: "매주",
+    D: "매일",
+    none: "반복안함"
+};
 
+var popupBackground = document.querySelector('.popupBackground');
+var popupContent = document.querySelector('.popupContent');
 var span = document.querySelector('.popupClose');
 
+// x클릭시 팝업 닫기
+span.addEventListener("click", closePopup);
+// 팝업 바깥영역 클릭시 팝업 닫기
+window.addEventListener("click", function(event) {
+    if (event.target.className === "popupBackground") {
+        closePopup();
+    }
+});
 // fc-content class content 클릭시 popup 출력
-document.addEventListener("click",function(event) {
-    if(event.target.className ==="fc-content"){
-    popup.style.display = "block";}
-} );
-span.addEventListener("click",function() {
-    popup.style.display = "none";
+document.addEventListener("click", function(event) {
+    if (event.target.className === "fc-content") {
+        showPopup();
+        getCoordinate(event);
+        insertPopupContent();
+    }
 });
 
-
-// x클릭시 팝업 닫기
-span.onclick = function() {
-    popup.style.display = "none";
-};
-
-// 팝업 바깥영역 클릭시 팝업 닫기
-window.onclick = function(event) {
-    if (event.target == popup) {
-        popup.style.display = "none";
-    }
-};
+function closePopup(){
+  popupBackground.style.display = "none";
+}
+function showPopup(){
+  popupBackground.style.display = "block";
+}
+// 클릭한 지점 좌표 반영
+function getCoordinate(event){
+  var x = event.clientX,
+      y = event.clientY;
+  popupContent.style.top = y + 'px';  // - popupContent.offsetHeight
+  popupContent.style.left = x + 'px';
+}
+function insertPopupContent(){
+  var parsedStart = schedule['start'].replace("T", " ");
+  var parsedEnd = schedule['end'].replace("T", " ");
+  var strdata = "<strong><%= title %></strong><br><label >시간: </label><span>" + parsedStart + " ~ " + parsedEnd + "</span><br><label >반복: </label><span>" + repeat[schedule['repeat']] + "<span><br><label >장소: </label><span><%= place %></span><br><label >설명: </label><span><%= desc %></span>";
+  var compiled = _.template(strdata);
+  var str = compiled(schedule);
+  document.querySelector('.parsedContent').innerHTML = str;
+}
