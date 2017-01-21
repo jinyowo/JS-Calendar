f696ce72f31df1e807fbf360fd613d64220db2b7
likelion-lulushu <iampoem315@gmail.com>
2017-01-21 20:38:54 +0900

    feat(popup_test):클릭위치에 따라 팝업표시 다르게함
    화면을 4분면으로 나눠서 팝업표시 다르게함.
    임의로 정한 일정object와 기존 template과 추가하여 테스트하였음.
    template의 css에서 z-index:9999를 z-index:10으로 수정 필요
    fc-content 와 fc-title 클릭시 이벤트 발생.
    일정 object에 대해서 상의할 필요가 있음.

--- template/popup_test/popup.js
+++ template/popup_test/popup.js
@@ -15,11 +15,9 @@ var repeat = {
     D: "매일",
     none: "반복안함"
 };
-
 var popupBackground = document.querySelector('.popupBackground');
 var popupContent = document.querySelector('.popupContent');
 var span = document.querySelector('.popupClose');
-
 // x클릭시 팝업 닫기
 span.addEventListener("click", closePopup);
 // 팝업 바깥영역 클릭시 팝업 닫기
@@ -30,31 +28,49 @@ window.addEventListener("click", function(event) {
 });
 // fc-content class content 클릭시 popup 출력
 document.addEventListener("click", function(event) {
-    if (event.target.className === "fc-content") {
+    if (confirmTarget(event)) {
         showPopup();
         getCoordinate(event);
         insertPopupContent();
     }
 });
-
-function closePopup(){
-  popupBackground.style.display = "none";
+function confirmTarget(event) {
+    if (event.target.className === "fc-content" || event.target.className === "fc-title") {
+        return true;
+    } else return false;
 }
-function showPopup(){
-  popupBackground.style.display = "block";
+function closePopup() {
+    popupBackground.style.display = "none";
 }
-// 클릭한 지점 좌표 반영
-function getCoordinate(event){
-  var x = event.clientX,
-      y = event.clientY;
-  popupContent.style.top = y + 'px';  // - popupContent.offsetHeight
-  popupContent.style.left = x + 'px';
+function showPopup() {
+    popupBackground.style.display = "block";
 }
-function insertPopupContent(){
-  var parsedStart = schedule['start'].replace("T", " ");
-  var parsedEnd = schedule['end'].replace("T", " ");
-  var strdata = "<strong><%= title %></strong><br><label >시간: </label><span>" + parsedStart + " ~ " + parsedEnd + "</span><br><label >반복: </label><span>" + repeat[schedule['repeat']] + "<span><br><label >장소: </label><span><%= place %></span><br><label >설명: </label><span><%= desc %></span>";
-  var compiled = _.template(strdata);
-  var str = compiled(schedule);
-  document.querySelector('.parsedContent').innerHTML = str;
+// 클릭한 지점 좌표 반영, 화면을 중심점을 기준으로 4분면으로 나워서 팝업 위치를 각각 다르게 함
+function getCoordinate(event) {
+    var centerPointX = (window.innerWidth) / 2;
+    var centerPointY = (window.innerHeight) / 2;
+    var x = event.clientX;
+    var y = event.clientY;
+    if (x <= centerPointX && y <= centerPointY) {
+        popupContent.style.top = y + 'px';
+        popupContent.style.left = x + 'px';
+    } else if (x > centerPointX && y <= centerPointY) {
+        popupContent.style.top = y + 'px';
+        popupContent.style.left = (x - popupContent.offsetWidth) + 'px';
+    } else if (x <= centerPointX && y > centerPointY) {
+        popupContent.style.top = (y - popupContent.offsetHeight) + 'px';
+        popupContent.style.left = x + 'px';
+    } else if (x > centerPointX && y > centerPointY) {
+        popupContent.style.top = (y - popupContent.offsetHeight) + 'px';
+        popupContent.style.left = (x - popupContent.offsetWidth) + 'px';
+    }
+}
+//팜업 상세일정 표시
+function insertPopupContent() {
+    var parsedStart = schedule['start'].replace("T", " ");
+    var parsedEnd = schedule['end'].replace("T", " ");
+    var stringData = "<strong><%= title %></strong><br><label >시간: </label><span>" + parsedStart + " ~ " + parsedEnd + "</span><br><label >반복: </label><span>" + repeat[schedule['repeat']] + "<span><br><label >장소: </label><span><%= place %></span><br><label >설명: </label><span><%= desc %></span>";
+    var compiled = _.template(stringData);
+    var str = compiled(schedule);
+    document.querySelector('.parsedContent').innerHTML = str;
 }
