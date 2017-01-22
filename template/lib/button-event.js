4430de6c95f52ed95bef5da7961f7b71b6a378f7
jiny <hjin421@gmail.com>
2017-01-22 22:53:20 +0900

    refactor(month-calendar,button-event): 함수 쪼개기

--- template/lib/button-event.js
+++ template/lib/button-event.js
@@ -3,6 +3,11 @@ var arrowButtons = document.querySelector(".fc-left .fc-button-group");
 var typeButtons = document.querySelector(".fc-right .fc-button-group");
 var todayButton = document.querySelector(".fc-left .fc-today-button");
 
+var calendarTypeButton = {
+    month : document.querySelector(".fc-month-button"),
+    week : document.querySelector(".fc-agendaWeek-button"),
+    day : document.querySelector(".fc-agendaDay-button"),
+}
 function registerButtonEvents() {
     // 공통 이벤트
     for(var i=0; i<buttons.length; i++) {
@@ -11,15 +16,21 @@ function registerButtonEvents() {
         buttons[i].addEventListener("mousedown", mousedownEvent);
         buttons[i].addEventListener("mouseup", mouseupEvent);
     }
-
     // 화살표 달력 이동
-    arrowButtons.addEventListener("click", moveMonth);
+    arrowButtons.addEventListener("click", moveCalendar);
     // 달력 형식 변경
     typeButtons.addEventListener("click", changeType);
     // today
     todayButton.addEventListener("click", returnToday);
 }
-
+function moveCalendar(evt) {
+    switch(MyDate.type) {
+        case "month": moveMonth(evt); break;
+        case "week": moveWeek(evt); break;
+        case "day": moveDay(evt); break;
+    }
+    setCalendar(MyDate.type);
+}
 function moveMonth(evt) {
     var prevArrowClass = "fc-prev-button";
     var nextArrowClass = "fc-next-button";
@@ -27,43 +38,53 @@ function moveMonth(evt) {
     var button = evt.target.closest("button");
 
     if(button.classList.contains(prevArrowClass)) {
-        myDate.month--;
+        MyDate.month--;
     } else if(button.classList.contains(nextArrowClass)) {
-        myDate.month++;
+        MyDate.month++;
     }
 
-    if(myDate.month < 0) { myDate.month = 11; myDate.year--;}
-    else if(myDate.month > 11) { myDate.month = 0; myDate.year++;}
+    if(MyDate.month < 0) { MyDate.month = 11; MyDate.year--;}
+    else if(MyDate.month > 11) { MyDate.month = 0; MyDate.year++;}
 
-    activeButton(todayButton);
-    setCalendar();
 }
+function moveWeek(evt){
 
+}
+function moveDay(evt) {
+
+}
 function changeType(evt) {
-    var typeSet = document.querySelectorAll(".fc-right button");
-    var calendarSet = document.querySelectorAll(".fc-view-container .fc-view");
-    var type = evt.target.closest("button");
-
-    hideCalendar();
-    for(var i=0; i<typeSet.length; i++) {
-        if(typeSet[i] === type) {
-            calendarSet[i].style.display = "block";
-            typeSet[i].classList.add("fc-state-active");
-        } else {
-            typeSet[i].classList.remove("fc-state-active");
-        }
-    }
-
+    var button = evt.target.closest("button");
+    MyDate.type = button.innerText;
+    setCalendar(MyDate.type);
 }
-
+function setTypeButton(type) {
+    inactiveAllTypeButton();
+    switch(type) {
+        case "month": calendarTypeButton.month.classList.add("fc-state-active"); break;
+        case "week": calendarTypeButton.week.classList.add("fc-state-active"); break;
+        case "day": calendarTypeButton.day.classList.add("fc-state-active"); break;
+    }
+}
+function inactiveAllTypeButton() {
+    calendarTypeButton.month.classList.remove("fc-state-active");
+    calendarTypeButton.week.classList.remove("fc-state-active");
+    calendarTypeButton.day.classList.remove("fc-state-active");
+}
+function isToday() {
+    if(MyDate.year !== Today.year || MyDate.month !== Today.month || MyDate.date !== Today.date) {
+        activeButton(todayButton);
+        return false;
+    } else {
+        inactiveButton(todayButton);
+        return true;
+    }
+}
 function returnToday(evt) {
-    var today = new Date();
-    var thisYear = today.getFullYear();
-    var thisMonth = today.getMonth();
-
-    inactiveButton(todayButton);
-    setMyDate(thisYear, thisMonth);
-    setCalendar();
+    if(!isToday()) {
+        setMyDate(Today.year, Today.month, Today.date);
+        setCalendar(MyDate.type);
+    }
 }
 function mouseoverEvent(evt) {
     var button = evt.target.closest("button");
--- template/lib/month-calendar.js
+++ template/lib/month-calendar.js
@@ -1,39 +1,73 @@
 // month이름, weekday이름, 각 달의 마지막 날짜를 저장한 배열
-var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var weekdayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var weekdayClassArray = ["fc-sun", "fc-mon", "fc-tue", "fc-wed", "fc-thu", "fc-fri", "fc-sat"];
 
-var myDate = {
-    month : -1,
+// 현재 보여지는 달력의 정보를 저장할 object
+var MyDate = {
     year : -1,
+    month : -1,
     date : -1,
+    type : "month",
+}
+var _today = new Date();
+var Today = {
+    year : _today.getFullYear(),
+    month : _today.getMonth(),
+    date : _today.getDate(),
+}
+var calendarType = {
+    month : document.querySelector(".fc-month-view"),
+    week : document.querySelector(".fc-basicWeek-view"),
+    day : document.querySelector(".fc-agendaDay-view"),
 }
 
+document.addEventListener("DOMContentLoaded", init);
+function init() {
+    setMyDate(Today.year, Today.month, Today.date);
+    setCalendar(MyDate.type);   // 기본 type "month"
+
+    registerButtonEvents();
+}
+function setCalendar(type) {
+    switch(type) {
+        case "month": drawMonthCalendar(); break;
+        case "week": drawWeekCalendar(); break;
+        case "day": drawDayCalendar(); break;
+    }
+    showCalendar(type);
+    setTypeButton(type);
+    isToday();
+}
+function setMyDate(year, month, date) {
+    MyDate.month = month;
+    MyDate.year = year;
+    MyDate.date = date;
+}
 function getLastDate(month) {
     if(month < 0){ month = 11;}
 
-    var lastDate = new Date(myDate.year, month+1, 0).getDate();
+    var lastDate = new Date(MyDate.year, month+1, 0).getDate();
     return lastDate;
 }
 
 // 월에 맞도록 달력에 숫자를 뿌리는 함수
-function setCalendar() {
+function drawMonthCalendar() {
     // 해당 년, 월의 1일 구하기
-    var firstDate = new Date(myDate.year, myDate.month, 1); // 이번달 1일
+    var firstDate = new Date(MyDate.year, MyDate.month, 1); // 이번달 1일
 
-    var lastDate = getLastDate(myDate.month);
+    var lastDate = getLastDate(MyDate.month);
     var firstWeekday = firstDate.getDay();
 
     // 상단 가운데 월이름, 년도 -TODO: 나중에 함수로 뺄 예정
     var monthTitle = document.querySelector(".fc-center");
-    var thisMonthFullname = monthArray[myDate.month];
-    monthTitle.innerHTML = "<h2>"+thisMonthFullname+" "+myDate.year+"</h2>";
+    var thisMonthFullname = monthArray[MyDate.month];
+    monthTitle.innerHTML = "<h2>"+thisMonthFullname+" "+MyDate.year+"</h2>";
 
     // 달력에 숫자 뿌리기 - TODO: 함수로 빼기
     var calendar = [];
 
-    var prevMonthLastDate = getLastDate(myDate.month-1);
-
+    var prevMonthLastDate = getLastDate(MyDate.month-1);
     var cells = document.querySelectorAll(".fc-month-view .fc-day-top");
 
     var prevMonthfirstDate = prevMonthLastDate - firstWeekday + 1;
@@ -41,7 +75,7 @@ function setCalendar() {
 
     // 지난달에 해당하는 날짜를 먼저 배열에 넣어준다.
     for(var i = prevMonthfirstDate; i<=prevMonthLastDate; i++) {
-        cells[currentDate].setAttribute("data-date", formDate(myYear, myMonth, i));
+        cells[currentDate].setAttribute("data-date", formDate(MyDate.year, MyDate.month, i));
         if(!cells[currentDate].className.includes("fc-other-month")) {
           cells[currentDate].className += " fc-other-month";
         }
@@ -50,7 +84,7 @@ function setCalendar() {
     }
     // 이번달에 해당하는 날짜를 추가로 배열에 넣어준다.
     for(var i = 1; i<=lastDate; i++) {
-        cells[currentDate].setAttribute("data-date", formDate(myYear, myMonth + 1, i));
+        cells[currentDate].setAttribute("data-date", formDate(MyDate.year, MyDate.month + 1, i));
         if(cells[currentDate].className.includes("fc-other-month")) {
             removeClass(cells[currentDate], "fc-other-month");
         }
@@ -64,7 +98,7 @@ function setCalendar() {
     for(var i=0; i<nums.length; i++) {
         // 다음달 날짜 채우기
         if(calendar[i] === undefined) {
-            cells[currentDate].setAttribute("data-date", formDate(myYear, myMonth + 2, nextMonthDate));
+            cells[currentDate].setAttribute("data-date", formDate(MyDate.year, MyDate.month + 2, nextMonthDate));
             if(!cells[currentDate].className.includes("fc-other-month")) {
                 cells[currentDate].className += " fc-other-month";
             }
@@ -75,38 +109,22 @@ function setCalendar() {
     }
 }
 
-// 해당 날짜에 대한 정보를 ISO형식으로 div의 data-date속성으로 저장
-function saveDate(date) {
+function drawWeekCalendar() {
 
 }
+function drawDayCalendar() {
 
-document.addEventListener("DOMContentLoaded", init);
-function init() {
-    // 오늘의 날짜를 받아옵니다.
-    var today = new Date();
-    var thisYear = today.getFullYear();
-    var thisMonth = today.getMonth(); // 0 ~ 11
-    var thisDate = today.getDate();
-
-    setMyDate(thisYear, thisMonth);
-    setCalendar();
-    var monthType = document.querySelector(".fc-right .fc-month-button");
-    monthType.classList.add("fc-state-active");
-    inactiveButton(todayButton);
-    hideCalendar();
-
-    document.querySelector(".fc-month-view").style.display = "block";
-
-    registerButtonEvents();
 }
-
-function setMyDate(year, month) {
-    myDate.month = month;
-    myDate.year = year;
+function showCalendar(type) {
+    hideAllCalendar();
+    switch(type) {
+        case "month": calendarType.month.style.display = "block"; break;
+        case "week": calendarType.week.style.display = "block"; break;
+        case "day": calendarType.day.style.display = "block"; break;
+    }
 }
-
-function hideCalendar() {
-    document.querySelector(".fc-month-view").style.display = "none";
-    document.querySelector(".fc-basicWeek-view").style.display = "none";
-    document.querySelector(".fc-agendaDay-view").style.display = "none";
+function hideAllCalendar() {
+    calendarType.month.style.display = "none";
+    calendarType.week.style.display = "none";
+    calendarType.day.style.display = "none";
 }
