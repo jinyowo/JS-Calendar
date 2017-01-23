8aa3783c36290feba9568ce97dc6de80b3991387
likelion-lulushu <iampoem315@gmail.com>
2017-01-23 17:52:07 +0900

    feat(lib): 날짜 click하면 일정 작성form으로 이동
    click한 칸의 날짜 정보를 받아서 localstorage를 통해서 form으로 전달

---
+++ template/lib/make-schedule.js
@@ -0,0 +1,19 @@
+var div = document.querySelector('.fc-widget-content');
+// 이벤트를 달력칸으로 한정
+div.addEventListener("click", getDateInfo);
+// 달력칸의 날짜 정보를 뽑아내고, 날짜 정보가 있다면 스케줄 작성 form으로 이동한다.
+function getDateInfo(event) {
+    var dateData = "";
+    dateData = event.target.getAttribute("data-date");
+    if (event.target.className === "fc-day-number") {
+        dateData = event.target.parentNode.getAttribute("data-date");
+    }
+    if (!!dateData) {
+        saveData(dateData);
+        window.location.href = "makeSchedule.html";
+    }
+}
+//클릭한 칸의 날짜 정보를 로컬스토리지에 저장한다.
+function saveData(dateData) {
+    localStorage.setItem("dateKey", dateData);
+}
