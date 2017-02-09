286eeef9bf69127298445cf0b9ec991241d36d4a
jiny <hjin421@gmail.com>
2017-02-09 14:20:48 +0900

    chore(data): 데모를 위한 데이터로 변경

--- template/js/model/data.js
+++ template/js/model/data.js
@@ -1,59 +1,51 @@
 var Data = {
     manyEvent : [{
-        "title": "제목1",
-        "place": "장소1",
-        "desc": "설명1",
-        "start": "2017-01-04T16:07:00Z",
-        "end": "2017-01-04T17:07:00Z",
+        "title": "발렌타인데이",
+        "place": "서울",
+        "desc": "여자친구랑",
+        "start": "2017-02-14T16:07:00Z",
+        "end": "2017-02-14T17:07:00Z",
         "allDay": false,
-        "repeat": "none"
+        "repeat": "Y"
     }, {
-        "title": "제목2",
-        "place": "장소2",
-        "desc": "설명2",
-        "start": "2017-01-04T16:08:00Z",
-        "end": "2017-01-04T17:08:00Z",
+        "title": "여자친구랑 1주년",
+        "place": "서울",
+        "desc": "1주년",
+        "start": "2017-02-14T16:08:00Z",
+        "end": "2017-02-14T17:08:00Z",
         "allDay": true,
-        "repeat": "W"
-    }, {
-        "title": "제목3",
-        "place": "장소3",
-        "desc": "설명3",
-        "start": "2017-01-04T18:08:00Z",
-        "end": "2017-01-04T22:08:00Z",
-        "allDay": false,
         "repeat": "none"
     }],
     longEvent : [{
-        title: "1/24~2/7",
-        start: "2017-01-24T00:00:00Z",
-        end: "2017-02-07T01:59:00Z",
-        allDay: "false",
-        repeat: "none",
-        place: "where",
-        desc: "dddddd"
+        title: "크리스마스",
+        start: "2016-12-25T00:00:00Z",
+        end: "2016-12-25T11:59:00Z",
+        allDay: true,
+        repeat: "Y",
+        place: "",
+        desc: ""
     }],
     repeatEventW : [{
-        title: "12/6: 화수목 매주반복",
-        start: "2016-12-06T00:00:00Z",
-        end: "2016-12-08T02:59:00Z",
-        allDay: "false",
-        repeat: "W",
-        place: "where",
-        desc: "dddddd"
+        title: "설 연휴",
+        start: "2017-01-27T00:00:00Z",
+        end: "2017-01-30T02:59:00Z",
+        allDay: true,
+        repeat: "none",
+        place: "",
+        desc: "30일 대체공휴일"
     }],
     repeatEventM : [{
-        title: "매월1일",
-        start: "2017-01-01T00:00:00Z",
-        end: "2017-01-01T02:59:00Z",
-        allDay: "false",
-        repeat: "M",
-        place: "where",
-        desc: "dddddd"
+        title: "화이트데이",
+        start: "2017-03-14T00:00:00Z",
+        end: "2017-03-14T02:59:00Z",
+        allDay: true,
+        repeat: "Y",
+        place: "",
+        desc: ""
     }],
 };
 
-localStorage.setItem("2017-01-04S2017-01-04E", JSON.stringify(Data.manyEvent));
-localStorage.setItem("2017-01-24S2017-02-07E", JSON.stringify(Data.longEvent));
-localStorage.setItem("2016-12-06S2016-12-08E", JSON.stringify(Data.repeatEventW));
-localStorage.setItem("2017-01-01S2017-01-01E", JSON.stringify(Data.repeatEventM));
+localStorage.setItem("2017-02-14S2017-02-14E", JSON.stringify(Data.manyEvent));
+localStorage.setItem("2016-12-25S2016-12-25E", JSON.stringify(Data.longEvent));
+localStorage.setItem("2017-01-27S2017-01-30E", JSON.stringify(Data.repeatEventW));
+localStorage.setItem("2017-03-14S2017-03-14E", JSON.stringify(Data.repeatEventM));
