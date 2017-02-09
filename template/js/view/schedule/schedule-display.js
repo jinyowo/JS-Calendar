f9430e5a12e2f0c9be72312739321f6c54910923
jiny <hjin421@gmail.com>
2017-02-09 10:17:53 +0900

    fix(repeat): 달력 끝에 반복일정이 걸쳐서 출력안되는 부분 수정
    #190

--- template/js/view/schedule/schedule-display.js
+++ template/js/view/schedule/schedule-display.js
@@ -244,7 +244,7 @@ ScheduleDisplay.prototype = {
 
         var last = Utility.setTimeByGMT(new Date(this.calendar.lastDay));
         Utility.setTimeDefault(last, 9);
-        while (last >= Utility.setTimeByGMT(nextEnd)) {
+        while (last >= Utility.setTimeByGMT(nextStart)) {
             this.showRepeatEvent(event, nextStart, nextEnd);
             this.moveNextRepeatEvent(nextStart, nextEnd, repeatType);
         }
